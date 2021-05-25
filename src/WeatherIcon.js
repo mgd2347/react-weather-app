83f66813a8ec474b206109e7ca57f9d87d3b3687
mgd2347 <margaridaf235@gmail.com>
2021-05-25 16:39:19 +0100

    Changed the background and font colour

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <div className="WeatherIcon">
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
-        color="#ffcc99"
+        color="#ffced9"
         size={100}
         animate={true}
       />
